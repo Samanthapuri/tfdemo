8fd311444ac7511f13eedca5be5b09823e8b1ef8
Samanthapuri <33673268+Samanthapuri@users.noreply.github.com>
2025-05-16 16:51:18 -0500

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -1,13 +1,7 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 resource "aws_s3_bucket" "example" {
   bucket = var.bucket != "" && var.bucket_prefix == "" ? var.bucket : null
   bucket_prefix = var.bucket == "" && var.bucket_prefix != "" ? var.bucket_prefix : null
+  force_destroy = var.force_destory == "" ? false : var.force_detsory
+  object_lock_enabled = var.object_lock_enabled == "" ? true : var.object_lock_enabled
+  tags = var.tags
 }
