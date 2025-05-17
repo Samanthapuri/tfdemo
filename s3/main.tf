55044f75fdb4b0688c6f5bdaf4d51f2165c0f628
Samanthapuri <33673268+Samanthapuri@users.noreply.github.com>
2025-05-16 20:21:49 -0500

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "example" {
   bucket = var.bucket != "" && var.bucket_prefix == "" ? var.bucket : null
   bucket_prefix = var.bucket == "" && var.bucket_prefix != "" ? var.bucket_prefix : null
-  force_destroy = var.force_destory == "" ? false : var.force_detsory
-  object_lock_enabled = var.object_lock_enabled == "" ? true : var.object_lock_enabled
+  force_destroy = var.force_destory
+  object_lock_enabled = var.object_lock_enabled
   tags = var.tags
 }
