a36d2a01e9bbfefc59403d7c4fd3056076770d23
Samanthapuri <33673268+Samanthapuri@users.noreply.github.com>
2025-06-12 17:47:10 -0500

    Update main.tf

--- outputtest/main.tf
+++ outputtest/main.tf
@@ -7608,5 +7608,10 @@ value = [  {
     "name": "vm-2",
     "size": "Standard_DS1_v2"
   },
+  {
+    "image": "UbuntuServer:18.04-LTS",
+    "name": "vm-2",
+    "size": "Standard_DS1_v2"
+  },
 ]
 }
