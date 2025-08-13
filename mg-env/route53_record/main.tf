7947e84eca69e2f7f2ca4b114e8c03f9ccf26bb8
Samanthapuri <33673268+Samanthapuri@users.noreply.github.com>
2025-08-12 19:25:12 -0500

    Update main.tf

--- mg-env/route53_record/main.tf
+++ mg-env/route53_record/main.tf
@@ -20,3 +20,20 @@ resource "aws_route53_record" "this" {
   ttl     = var.ttl
   zone_id = data.aws_route53_zone.this.zone_id
 }
+
+resource "aws_route53_record" "this" {
+  count   = var.instance_count
+  name    = var.instance_dns_names[count.index]
+  records = split(",", var.records[count.index])
+  type    = var.type
+  ttl     = var.ttl
+  zone_id = data.aws_route53_zone.this.zone_id
+  name    = var.load_balancer_dns_record_name                          
+  type    = "A"                                          
+
+      alias {
+        name                   = var.load_balancer_dns_name   
+        zone_id                = var.load_balancer_zone_id    
+        evaluate_target_health = false                              
+      }
+}
