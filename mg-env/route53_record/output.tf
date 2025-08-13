80e5178f20958354537c6313057f0df21e5dad52
Samanthapuri <33673268+Samanthapuri@users.noreply.github.com>
2025-08-13 06:18:08 -0500

    Update output.tf

--- mg-env/route53_record/output.tf
+++ mg-env/route53_record/output.tf
@@ -2,3 +2,8 @@ output "fqdn" {
   description = "Fully qualified domain name"
   value = aws_route53_record.this.*.fqdn
 }
+
+output "lb_fqdn" {
+  description = "Fully qualified domain name for load balancer"
+  value = aws_route53_record.lb.fqdn
+}
