3c8ae48ce9f886f33ba19dea88ea515296210e4b
Samanthapuri <33673268+Samanthapuri@users.noreply.github.com>
2026-04-22 14:40:28 -0500

    Update main.tf

--- outputtest/main.tf
+++ outputtest/main.tf
@@ -2,7 +2,7 @@ variable "name" {
 }
 resource "null_resource" "example" {
 triggers = {
-always_run = “${timestamp()}”
+always_run = "${timestamp()}"
 }
   provisioner "local-exec" {
     command = "sleep 600"
