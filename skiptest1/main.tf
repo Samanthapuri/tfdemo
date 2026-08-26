ff7386cf0b7583232414b8a37b1dcd2922ff7c17
Samanthapuri <33673268+Samanthapuri@users.noreply.github.com>
2026-08-26 11:02:40 -0500

    Add initial Terraform configuration in main.tf

---
+++ skiptest1/main.tf
@@ -0,0 +1,14 @@
+variable "name2" {
+}
+resource "null_resource" "example" {
+triggers = {
+always_run = "${timestamp()}"
+}
+  provisioner "local-exec" {
+    command = "echo hi"
+  }
+}
+
+output "test2" {
+value = "test3"
+}
