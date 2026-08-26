94789387c70dcd2d3333e4b1d7c2ab8dedb64238
Samanthapuri <33673268+Samanthapuri@users.noreply.github.com>
2026-08-25 21:44:11 -0500

    Add filename variable and update output handling
    Added a filename variable and updated command to write output to a file. Introduced a local_file data source to read the content of the generated file.

--- outputtest/main.tf
+++ outputtest/main.tf
@@ -1,11 +1,18 @@
-variable "name" {
+variable "filename" {
+default = "result.txt"
 }
+
 resource "null_resource" "example" {
   provisioner "local-exec" {
-    command = "echo hi"
+    command = "echo hi > ${path.module}/${var.filename}"
   }
 }
 
+data "local_file" "result" {
+  filename   = "${path.module}/result.txt"
+  depends_on = [null_resource.example]
+}
+
 output "test" {
-value = "test"
+  value = data.local_file.result.content
 }
