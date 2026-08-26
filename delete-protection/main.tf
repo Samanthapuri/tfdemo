2cfac746dbf22c20709a166a55272c11aa8f64e5
Samanthapuri <33673268+Samanthapuri@users.noreply.github.com>
2026-08-26 13:41:14 -0500

    Change destroy variable default to 'yes' and update logic

--- delete-protection/main.tf
+++ delete-protection/main.tf
@@ -1,18 +1,22 @@
 variable "destroy" {
 
-default = "false"
+default = "yes"
 
 }
 
+locals {
+  destroy_allowed = var.destroy == "yes"
+}
+
 resource "null_resource" "protected" {
-  count = var.destroy == "false" ? 0 : 1
+  count = local.destroy_allowed ? 0 : 1
   lifecycle {
     prevent_destroy = true
   }
 }
 
 resource "null_resource" "unprotected" {
-  count = var.destroy == "false" ? 1 : 0
+  count = local.destroy_allowed ? 1 : 0
   lifecycle {
     prevent_destroy = false
   }
